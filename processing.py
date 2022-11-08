37ac7ead6c0922c99aff648115afcbeb350be66c
drumph <drumph@caltech.edu>
2022-11-07 18:26:20 -0800

    Get multiprocessing ProcessSteps working
    Includes lots of bug fixes

--- processing.py
+++ processing.py
@@ -1,5 +1,5 @@
 # processing.py
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Callable
 from enum import Enum
 import multiprocessing as mp
 import logging
@@ -66,21 +66,12 @@ class ProcessStep():
         This allows this processing step's end outputs to be
         retrieved.
         """
+        return self._endOutputs
 
-    def run(self) -> None:
+    def run(self, progressCallback: Callable[[int, str], None] = None) -> None:
         """
         Run the process step, consuming the inputs and
         producing one or both of two types of outputs
-
-        The derived class must implement this
-        """
-        raise NotImplementedError()
-
-    def progressCallback(self) -> Tuple[int, str]:
-        """
-        Returns a tuple of two items:
-            Percent complete (0 - 100)
-            The name of the step currently running (or just completed)
 
         The derived class must implement this
         """
@@ -98,14 +89,25 @@ class ProcessStepSequence(ProcessStep):
         self.baseStepName = "Sequence"
         self._stepName = self.baseStepName
         self.onStep = 0
+        self._stepsCompleted = 0
+        self._progressCallback = None
 
-    def run(self) -> None:
+    def progressCallbackWrapper(self, progress: int, stepName: str):
+        # Account for process steps already completed when reporting progress
+        if self._progressCallback:
+            self._progressCallback(
+                int((self._stepsCompleted + progress / 100.) / len(self._steps)),
+                self.baseStepName + '.' + stepName)
+
+    def run(self, progressCallback: Callable[[int, str], None] = None) -> None:
+        self._progressCallback = progressCallback
+        self._stepsCompleted = 0
         stepData = self._inputs
         self._status = ProcessStatus.RUNNING
         for onStep, step in enumerate(self._steps):
             self._stepName = f"{self.baseStepName}.{step.stepName()}"
             step.setInput(stepData)
-            step.run()
+            step.run(self.progressCallbackWrapper if progressCallback else None)
             if step.status() != ProcessStatus.COMPLETED:
                 self._status = step.status()
                 return
@@ -115,26 +117,18 @@ class ProcessStepSequence(ProcessStep):
         self._stepOutputs = stepData
         self._status = status
         self._stepName = self.baseStepName
-
-    def progressCallback(self) -> Tuple[int, str]:
-        """
-        Return progress percent (0 - 100) and name of processStep
-        Calculate progress as follows:
-        progress = (100 * stepsCompleted + stepProgress) / totalSteps
-        processStep = "Sequential.<substepName>" (Updated within run())
-        """
-        divisor = len(self._steps)
-        stepProgress, _ = self._steps(self.onStep).progressCallback()
-        progress = int(100 * self.onStep + stepProgress) / divisor
-        return (progress, self._stepName)
+        progressCallback(100, self._stepName)
 
 class ProcessStepConcurrent(ProcessStep):
     """
     A process step composed of individual steps that can
     be run in parallel.
+
+    params, if supplied, can be either a dict or a list of dicts
     """
 
-    def __init__(self, step: ProcessStep, params: Dict = {}):
+    def __init__(self, step: ProcessStep, params = {}):
+        assert isinstance(params, (dict, list))
         super().__init__(params)
         self._stepName = "Concurrent"
         self._step = step
@@ -144,25 +138,27 @@ class ProcessStepConcurrent(ProcessStep):
         self._stepOutputs = [None] * len(self._inputs)
         self._endOutputs = [None] * len(self._inputs)
 
-    def runConcurrentInner(self, stepClass: ProcessStep, params: Dict, inQ: mp.Queue, stepOutQ: mp.Queue, endOutQ: mp.Queue) -> None:
+    def runInner(self, stepClass: ProcessStep, params: Dict, inQ: mp.Queue, outQ: mp.Queue) -> None:
         """
         Pull the inputs off the input queue and call the processStep's run() function
         Get the outputs and pushd them on the two output queues.
         """
-        while True:
-            step = stepClass(params)
-            idx, inputs = inQ.get()
-            if idx < 0:
-                # pass the "poison pill" on and then exit
-                stepOutQ.put((idx, None))
-                endOutQ.put((idx, None))
-                break
-            step.setInputs(inputs)
-            step.run()
-            stepOutQ.put((idx, step.stepOutputs()))
-            endOutQ.put((idx, step.endOutputs()))
+        logger = mp.log_to_stderr()
+        try:
+            while True:
+                step = stepClass(params)
+                idx, inputs = inQ.get()
+                if idx < 0:
+                    # pass the "poison pill" on and then exit
+                    outQ.put((idx, None))
+                    break
+                step.setInputs(inputs)
+                step.run()
+                outQ.put((idx, step.stepOutputs(), step.endOutputs()))
+        except Exception as e:
+            logger.exception(f"Worker {getpid()} got exception {e}")
 
-    def accumulateOutputs(self, workers: int, stepOutQ: mp.Queue, endOutQ: mp.Queue, finalStepOutQ: mp.Queue, finalEndOutQ: mp.Queue) -> None:
+    def accumulateOutputs(self, workers: int, outQ: mp.Queue, finalOutQ: mp.Queue) -> None:
         """
         Accumulate the results from the various parallel threads and accumulate them.
         This should be the last step of the pipeline
@@ -171,30 +167,28 @@ class ProcessStepConcurrent(ProcessStep):
         """
         logger = mp.log_to_stderr()
         logger.setLevel(logging.INFO)
-        unorderedStepOutputs = []
-        unorderedEndOutputs = []
+        unorderedOutputs = []
         workersDone = 0
-        while workersDone < workers:
-            stepIdx, stepOutputs = stepOutQ.get()
-            endIdx, endOutputs = endOutQ.get()
-            if stepIdx != endIdx:
-                workersDone = workers
-                raise ValueError(f"stepIdx ({stepIdx}) != endIdx ({endIdx})")
-            if stepIdx < 0:
-                workersDone += 1
-                logger.info(f"{workersDone} workers done so far")
-                continue
-            unorderedStepOutputs.append((stepIdx, stepOutputs))
-            logger.info(f"Appended idx {stepIdx} to step outputs, shape is {stepOutputs[0].shape}")
-            unorderedEndOutputs.append((endIdx, endOutputs))
-        unorderedStepOutputs.sort(key=lambda item: item[0])
-        unorderedEndOutputs.sort(key=lambda item: item[0])
-        logger.info(f"len(unorderedEndOutputs): {len(unorderedEndOutputs)}")
-        # remove the indices and unwrap each slice before outputting
-        finalStepOutQ.put([item[1][0] for item in unorderedStepOutputs])
-        finalEndOutQ.put([item[1][0] for item in unorderedEndOutputs])
+        try:
+            while workersDone < workers:
+                outputs = outQ.get()
+                idx = outputs[0]
+                if idx < 0:
+                    workersDone += 1
+                    logger.info(f"{workersDone} workers done so far")
+                    continue
+                unorderedOutputs.append(outputs)
+                logger.info(f"Appended idx {idx} to outputs")
+            unorderedOutputs.sort(key=lambda item: item[0])
+            # remove the indices and unwrap each slice before outputting
+            # create a tuple of stepOutputs and endOutputs
+            finalOutQ.put((
+                [item[1][0] for item in unorderedOutputs],
+                [item[2][0] for item in unorderedOutputs]))
+        except Exception as e:
+            logger.exception(f"accumulateOutputs got exception {e}")
 
-    def run(self) -> None:
+    def run(self, progressCallback: Callable[[int, str], None]) -> None:
         """
         Run the steps concurrently, accumulating the results in a list
 
@@ -202,57 +196,97 @@ class ProcessStepConcurrent(ProcessStep):
         """
         logger = mp.log_to_stderr()
         logger.setLevel(logging.INFO)
+        self._status = ProcessStatus.RUNNING
+        self._stepOutputs = []
+        self._endOutputs = []
         nCores = mp.cpu_count()
-        coresToUse = max(2, int(nCores * 3 / 4))
+        # We need at least two so that accumulateOutputs can run alongside runInner
+        # Use up to 3/4 of the available cores, but no need for more than we have work.
+        coresToUse = min(max(2, int(nCores * 3 / 4)), len(self._inputs) + 1)
+        nWorkers = coresToUse - 1
         logger.info(f"Using {coresToUse} cores")
         with mp.Pool(processes=coresToUse) as pool:
-            mgr = mp.Manager()
-            inQ = mgr.Queue(coresToUse)
-            stepOutQ = mgr.Queue(5)
-            endOutQ = mgr.Queue(5)
-            finalStepOutQ = mgr.Queue(1)
-            finalEndOutQ = mgr.Queue(1)
-            logger.info("Queues created")
-            workers = [pool.apply_async(self.runConcurrentInner, (self._step, self._params, inQ, stepOutQ, endOutQ)) for i in range(coresToUse-1)]
-            logger.info(f"Started {len(workers)} Workers with runConcurrentInner")
-            accumulator = pool.apply_async(self.accumulateOutputs, (len(workers), stepOutQ, endOutQ, finalStepOutQ, finalEndOutQ))
-            logger.info("Started accumulateOutputs Worker")
+            with mp.Manager() as mgr:
+                inQ = mgr.Queue(nWorkers)   # one per worker
+                outQ = mgr.Queue(nWorkers)  # contains tuple(stepOutputs, endOutputs) to avoid race with two queues
+                finalOutQ = mgr.Queue(1)
+                logger.info("Queues created")
+                if isinstance(self._params, dict):
+                    self._params = [self._params] * nWorkers
+                assert len(self._params) == nWorkers    # check in case params was passed in as a list
+                workers = [pool.apply_async(self.runInner, (self._step, self._params[i], inQ, outQ)) for i in range(nWorkers)]
+                logger.info(f"Started {nWorkers} Workers with runConcurrentInner")
+                accumulator = pool.apply_async(self.accumulateOutputs, (nWorkers, outQ, finalOutQ))
+                logger.info("Started accumulateOutputs Worker")
 
-            for idx, inputs in enumerate(self._inputs):
-                logger.info(f"Enqueued idx {idx}")
-                inQ.put((idx, inputs))
+                for idx, inputs in enumerate(self._inputs):
+                    logger.info(f"Enqueued idx {idx}")
+                    inQ.put((idx, [inputs]))
 
-            for idx in range(len(workers)):
-                logger.info(f"Enqueuing poison pill #{idx}")
-                inQ.put((-1, None))
+                for idx in range(nWorkers):
+                    logger.info(f"Enqueuing poison pill #{idx}")
+                    inQ.put((-1, None))
 
-            # close the pool's input and wait for everything to finish
-            pool.close()
-            logger.info("Pool closed")
-            pool.join()
-            logger.info("Pool joined")
-            try:
-                self._stepOutputs = finalStepOutQ.get_nowait()
-            except mp.queues.Empty as e:
-                print(f"runConcurrent: finalStepOutQ was unexpectedly empty")
-            try:
-                self._endOutputs = finalEndOutQ.get_nowait()
-            except mp.queues.Empty as e:
-                print(f"runConcurrent: finalEndOutQ was unexpectedly empty")
+                # close the pool's input and wait for everything to finish
+                pool.close()
+                logger.info("Pool closed")
+                pool.join()
+                logger.info("Pool joined")
+                try:
+                    stepOutputs, endOutputs = finalOutQ.get(True, 1)
+                except mp.queues.Empty:
+                    print("outputs unexpectedly empty")
+                self._stepOutputs.append(stepOutputs)
+                self._endOutputs.append(endOutputs)
+            if progressCallback:
+                #TODO: provide a callback mechanism that passes on updates
+                # from accumulateOutputs
+                progressCallback(100, self._stepName)
+            self._status = ProcessStatus.COMPLETED
 
-    def runSequential(self) -> None:
+class ProcessStepIterate(ProcessStep):
+    """
+    Execute sequentially a set of parallel paths of the overall process
+    This accumulates the outputs of each pass over the sequential inputs
+    in a list
+
+
+    """
+
+    def __init__(self, stepClass: ProcessStep, paramsList: List = {}):
+        super().__init__(None)
+        self._paramsList = paramsList
+        self._stepClass: ProcessStep = stepClass
+        self._stepName = "Iterate"
+        self._stepsCompleted: int = 0
+        self._progressCallback = None
+
+    def progressCallbackWrapper(self, progress: int, stepName: str):
+        # Account for process steps already completed when reporting progress
+        if self._progressCallback:
+            self._progressCallback(
+                int((self._stepsCompleted + progress / 100.) / len(self._inputs)),
+                self._stepName + '.' + stepName)
+
+    def run(self, progressCallback: Callable[[int, str], None] = None) -> None:
         """
-        Run the steps concurrently, and accumulate the results in a list
+        Run the steps sequentially, and accumulate the results in a list
         """
-        # start with a simple, sequential implementation
-        statuses: List(ProcessStatus) = [ProcessStatus.QUEUED] * len(self._steps)
-        for i, step in enumerate(self._steps):
-            step.setInputs([self._inputs[i]])
-            step.run()
+        assert len(self._inputs) == len(self._paramsList)
+        self._progressCallback = progressCallback
+        self._stepOutputs = []
+        self._endOutputs = []
+        self._stepsCompleted = 0
+        statuses: List(ProcessStatus) = [ProcessStatus.QUEUED] * len(self._inputs)
+        for i, input in enumerate(self._inputs):
+            step = self._stepClass(self._paramsList[i])
+            step.setInputs([input])
+            step.run(self.progressCallbackWrapper if progressCallback else None)
             statuses[i] = step.status()
-            self._stepOutputs[i] = step.stepOutputs()[0]
-            self._endOutputs[i] = step.endOutputs()[0]
-        if statuses.count(ProcessStatus.COMPLETED) == len(self._steps):
+            self._stepOutputs.append(step.stepOutputs()[0])
+            self._endOutputs.append(step.endOutputs()[0])
+            self._stepsCompleted = i+1
+        if statuses.count(ProcessStatus.COMPLETED) == len(self._inputs):
             self._status = ProcessStatus.COMPLETED
         elif statuses.count(ProcessStatus.ABORTED) > 0:
             self._status = ProcessStatus.ABORTED
@@ -262,17 +296,3 @@ class ProcessStepConcurrent(ProcessStep):
             raise SystemError("Parallel process should not still be running")
         else:
             self._status = ProcessStatus.REJECTED
-
-    def progressCallback(self) -> Tuple[int, str]:
-        """
-        Return progress percent (0 - 100) and name of processStep
-        Calculate progress as follows:
-        progress = sum(each step's progress) / totalSteps
-        processStep = "Concurrent"
-        """
-        divisor = len(self._steps)
-        totalProgress = 0
-        for step in self._steps:
-            stepProgress, _ = step.progressCallback()
-            totalProgress += stepProgress
-        return (int(totalProgress / divisor), self._stepName)
