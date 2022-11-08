ebeb09e49fed33c64a2b4ca11cc4ced5f48d7ccf
drumph <drumph@caltech.edu>
2022-11-07 18:32:47 -0800

    Implement runBatch using a  ProcessSteps sequence
    Test Settings is broken at the moment.

--- findSpotsTool.py
+++ findSpotsTool.py
@@ -4,27 +4,23 @@ from qtpy.QtWidgets import QApplication, QDialog, QDialogButtonBox, QFileDialog,
 from findSpotsTool_ui import Ui_MainWindow
 from imageCompareDialog import ImageCompareDialog
 from runnables.runnableDenoise import RunnableDenoise
+from algorithms.denoise import ProcessStepDenoiseConcurrent
+from algorithms.detect_spots import ProcessStepDetectSpotsConcurrent
+from algorithms.tripletDetection import ProcessStepFindTriplets
+from algorithms.touchingAnalysis import ProcessStepAnalyzeTouching, write_output
 from algorithms.find_spots import get_param
 from algorithms.confocal_file import ConfocalFile
 from algorithms.detect_spots import detect_spots
-from os.path import expanduser
+from processing import ProcessStatus, ProcessStep, ProcessStepIterate
+from os.path import expanduser, splitext
+import tifffile as tiff
+from PIL import Image
+from matplotlib import cm
 from enum import Enum
 import sys
 import time
-from typing import Dict
+from typing import Dict, List
 
-class ProcessingStep():
-    """
-    Class that encodes one processing step, which may be a composition
-    of other steps.
-    """
-
-    def __init__(self, stepName: str):
-        pass
-
-    def pickleToDict(self) -> Dict:
-        pickled = {}
-        return pickled
 class Worker(QRunnable):
     """
     Class to execute image processing currently with UI
@@ -41,6 +37,12 @@ class Worker(QRunnable):
 
 class FindSpotsTool(QMainWindow):
 
+    fileNameNone = '(none)'
+    noteProgressChanged = Signal(int, str)
+
+    testSettingsPipeline = [
+        (ProcessStepDenoiseConcurrent, [])
+    ]
     def __init__(self):
         super().__init__()
 
@@ -52,7 +54,7 @@ class FindSpotsTool(QMainWindow):
         self.pendingFilesModel = QStringListModel()
         self.ui.pendingFilesListView.setModel(self.pendingFilesModel)
         self.currentlyProcessingFile = None
-        self.ui.activeFileLineEdit.insert("(none)")
+        self.ui.activeFileLineEdit.insert(self.fileNameNone)
         self.completedFilesModel = QStringListModel()
         self.ui.completedFilesListView.setModel(self.completedFilesModel)
 
@@ -116,16 +118,150 @@ class FindSpotsTool(QMainWindow):
     @Slot(bool)
     def runBatch(self, checked: bool = False):
         while len(self.pendingFilesModel.stringList()) > 0:
-            self.processNextFile(False)
+            self.processNextFile()
 
-    def processNextFile(self, validateParams: bool):
-        pendingFilesList = self.pendingFilesModel.stringList()
-        if self.running or len(pendingFilesList) == 0:
+    def processNextFile(self):
+        # There may be a file currently being processed, where the user
+        # rejected the params for one of the process steps.  We need to
+        # restart processing that file with the process step that was
+        # rejected.
+
+        def progressCallback(progress: int, stepName: str) -> None:
+            self.noteProgressChanged.emit(progress, stepName)
+
+        fileToRun = self.ui.activeFileLineEdit.text()
+        if fileToRun == None or fileToRun == "" or fileToRun == self.fileNameNone:
+            pendingFilesList = self.pendingFilesModel.stringList()
+            if self.running or len(pendingFilesList) == 0:
+                return
+            fileToRun = pendingFilesList[0]
+            self.ui.activeFileLineEdit.setText(fileToRun)
+            pendingFilesList = pendingFilesList[1:]
+            self.pendingFilesModel.setStringList(pendingFilesList)
+
+        # open confocal file and get image
+        try:
+            cf = ConfocalFile(fileToRun)
+        except Exception as e:
+            QMessageBox.warning(self, "Invalid File", f"Image file {fileToRun} could not be opened.  Error was: {e}")
             return
-        fileToRun = pendingFilesList[0]
-        self.ui.activeFileLineEdit.setText(fileToRun)
-        pendingFilesList = pendingFilesList[1:]
-        self.pendingFilesModel.setStringList(pendingFilesList)
+        scale = cf.get_scale()
+
+        # set up the progress bar
+        self.ui.progressBar.setMinimum(0)
+        self.ui.progressBar.setMaximum(100)
+        self.ui.progressBar.setValue(0)
+
+        # set up the processing sequence and params
+        perChannelParamsList = [
+            # params for 3'CRM channel
+            {
+                'firstSlice': int(self.ui.firstSliceLineEdit.text()),
+                'lastSlice': int(self.ui.lastSliceLineEdit.text()),
+                'sigma': int(self.ui.sigma3CRMLineEdit.text()),
+                'sharpen': float(self.ui.sharpen3CRMLineEdit.text()),
+                'spot_detect_threshold': float(self.ui.spotDetection3CRMThresholdLineEdit.text())
+            },
+            # params for PPE channel
+            {
+                'firstSlice': int(self.ui.firstSliceLineEdit.text()),
+                'lastSlice': int(self.ui.lastSliceLineEdit.text()),
+                'sigma': int(self.ui.sigmaPPELineEdit.text()),
+                'sharpen': float(self.ui.sharpenPPELineEdit.text()),
+                'spot_detect_threshold': float(self.ui.spotDetectionPPEThresholdLineEdit.text())
+            },
+            # params for 5'CRM channel
+            {
+                'firstSlice': int(self.ui.firstSliceLineEdit.text()),
+                'lastSlice': int(self.ui.lastSliceLineEdit.text()),
+                'sigma': int(self.ui.sigma5CRMLineEdit.text()),
+                'sharpen': float(self.ui.sharpen5CRMLineEdit.text()),
+                'spot_detect_threshold': float(self.ui.spotDetection5CRMThresholdLineEdit.text())
+            }
+        ]
+        tripletsParams: Dict = {}
+        touchingParams: Dict = {
+            'touching_threshold': float(self.ui.touchingThresholdLineEdit.text())
+        }
+
+        processSequence: List[ProcessStep] = [
+                ProcessStepIterate(ProcessStepDenoiseConcurrent, perChannelParamsList),
+                ProcessStepDetectSpotsConcurrent(perChannelParamsList),
+                ProcessStepFindTriplets(scale, tripletsParams),
+                ProcessStepAnalyzeTouching(touchingParams)
+            ]
+
+        stepOutputs = [cf.channel_3CRM(), cf.channel_PPE(), cf.channel_5CRM()]
+        endOutputs = []
+        for step in processSequence:
+            step.setInputs(stepOutputs)
+            step.run(progressCallback)
+            if step.status() != ProcessStatus.COMPLETED:
+                msgBox = QMessageBox()
+                msgBox.exec()
+                return
+            stepOutputs = step.stepOutputs()
+            endOutputs.append(step.endOutputs())
+        output = stepOutputs[0]
+        conformance = endOutputs[-1][0]
+
+        outStem, _ = splitext(fileToRun)
+        write_output(output, outStem + "_results.txt")
+
+        # construct a new rgb version of the antibody image volume
+        gray_colormap = cm.get_cmap('gray', 256)
+        antibody_rgb = gray_colormap(cf.channel_antibody(), bytes=True)[:,:,:,0:3]
+
+        # Now plot each of the triplets into the image stack, colored by conformation
+        colors = {
+            '000': (255,   0,   0),     # red
+            '100': (  0,   0, 255),     # blue
+            '010': (  0, 255,   0),     # green
+            '001': (  0, 255, 255),     # cyan
+            '110': (255, 255,   0),     # yellow
+            '101': (  0,   0,   0),     # black
+            '011': (255, 255, 255),     # white
+            '111': (255,   0, 255)      # magenta
+            }
+
+        for triplet in output:
+            x = round(triplet[0] / scale['X'])
+            y = round(triplet[1] / scale['Y'])
+            z = round(triplet[2] / scale['Z'])
+            color = colors[triplet[3]]
+            for dx in range(-6,7):
+                if x + dx < 0 or x + dx >= antibody_rgb.shape[1]:
+                    continue
+                for dy in range(-6, 7):
+                    if y + dx < 0 or y + dy >= antibody_rgb.shape[2]:
+                        continue
+                    for dz in range(-2, 3):
+                        if z + dz < 0 or z + dz >= antibody_rgb.shape[0]:
+                            continue
+                        antibody_rgb[z+dz][x+dx][y+dy] = color
+        tiff.imwrite(outStem + "_rgb.tiff", antibody_rgb)
+
+        completedFilesList = self.completedFilesModel.stringList()
+        completedFilesList.append(fileToRun)
+        self.ui.activeFileLineEdit.setText(self.fileNameNone)
+        self.ui.progressBar.reset()
+        self.completedFilesModel.setStringList(completedFilesList)
+        self.running = False
+
+    def processNextFileOld(self, validateParams: bool):
+        # There may be a file currently being processed, where the user
+        # rejected the params for one of the process steps.  We need to
+        # restart processing that file with the process step that was
+        # rejected.
+        fileToRun = self.ui.activeFileLineEdit.text()
+        if fileToRun == None or fileToRun == "":
+            pendingFilesList = self.pendingFilesModel.stringList()
+            if self.running or len(pendingFilesList) == 0:
+                return
+            fileToRun = pendingFilesList[0]
+            self.ui.activeFileLineEdit.setText(fileToRun)
+            pendingFilesList = pendingFilesList[1:]
+            self.pendingFilesModel.setStringList(pendingFilesList)
 
         # open confocal file and get image
         try:
@@ -207,6 +343,10 @@ class FindSpotsTool(QMainWindow):
         self.completedFilesModel.setStringList(completedFilesList)
         self.running = False
 
+    @Slot(int, str)
+    def progressChanged(self, progress: int, stepName: str) -> None:
+        self.ui.progressBar.setValue(progress)
+        self.ui.stepNameLineEdit.setText(stepName)
 
     @Slot()
     def clearCompletedFiles(self):
