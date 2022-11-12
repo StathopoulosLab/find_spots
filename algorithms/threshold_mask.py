0ef337023bc6e21ebaeb3d5c22f3067455af6cd3
drumph <drumph@caltech.edu>
2022-11-11 21:52:49 -0800

    Add threshold_mask functionality
    This generates a mask from an image volume, typically the nucleus channel of a
    confocal microscope output, and applies it to the other channels in order to
    mask out non-nuclear regions, where fluorescent regions of DNA should never occur.

---
+++ algorithms/threshold_mask.py
@@ -0,0 +1,65 @@
+# threshold_mask.py
+
+from qtpy.QtWidgets import QApplication
+import cv2
+from processing import ProcessStatus, ProcessStep
+import numpy as np
+from typing import Callable, Dict, List
+
+class ProcessStepThresholdMask(ProcessStep):
+    """
+    Mask out regions of an image based on a thresholded mask of a different image.
+    In spot finding, this can be used to eliminate spots that lie outside the nucleus.
+    """
+
+    def __init__(self, params: Dict = {}):
+        super().__init__(params)
+        self._stepName = "ThresholdMask"
+
+    def run(self, progressCallback: Callable[[int, str], None] = None):
+        assert isinstance(self._inputs, list) and len(self._inputs) > 1
+        assert 'nucleus_mask_threshold' in self._params
+        self._status = ProcessStatus.RUNNING
+        nucleus_mask_threshold = self._params.get('nucleus_mask_threshold')
+        self._stepOutputs = []
+        self._endOutputs = []
+        maskImage = self._inputs[-1]
+        if maskImage.dtype != np.uint8:
+            maskImage = np.uint8(maskImage)
+        if progressCallback:
+            progressCallback(0, self._stepName)
+        if self._app:
+            self._app.processEvents()
+        for input in self._inputs[0:-1]:
+            if input.dtype != np.uint8:
+                input = np.uint8(input)
+            if len(input.shape) > 2:
+                firstSlice = self._params['firstSlice'] if 'firstSlice' in self._params else 0
+                lastSlice = self._params['lastSlice'] if 'lastSlice' in self._params else -1
+                totalSlices = input.shape[0]
+                firstSlice = max(0, min(totalSlices, firstSlice))
+                lastSlice = min(totalSlices, totalSlices + lastSlice if lastSlice < 0 else lastSlice)
+                slices = [input[i] for i in range(firstSlice, lastSlice+1)]
+                maskImageSlices = [maskImage[i] for i in range(firstSlice, lastSlice+1)]
+            else:
+                slices = input
+                maskImageSlices = maskImage
+            maskedSlices = []
+            thresholdsUsed = []
+            for slice, maskImageSlice in zip(slices, maskImageSlices):
+                thresholdUsed, maskSlice = cv2.threshold(maskImageSlice, nucleus_mask_threshold, 255, cv2.THRESH_BINARY)
+                thresholdsUsed.append(thresholdUsed)
+                maskedSlices.append(np.bitwise_and(slice, maskSlice))
+            maskedOutput = np.array(maskedSlices, dtype=np.uint8).squeeze()
+            self._stepOutputs.append(maskedOutput)
+            self._endOutputs.append(thresholdsUsed)
+            if progressCallback:
+                progressCallback(int(100 / (len(self._inputs) - 1)), self._stepName)
+            if self._app:
+                self._app.processEvents()
+        self._status = ProcessStatus.COMPLETED
+
+class ProcessStepThresholdMaskConcurrent(ProcessStep):
+    """
+    """
+
